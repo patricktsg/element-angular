4c964ffe5a247222e2507d12d8f4c399ce3aa0cd
Omar Yepez <oyepez003@gmail.com>
2020-01-30 14:51:17 -0500

    feat: add bundle and release folders

---
+++ release/input/help.d.ts
@@ -0,0 +1,7 @@
+/**
+ *  initial file from Element
+ *  see: github.com/ElemeFE/element/blob/dev/packages/input/src/calcTextareaHeight.js
+ *
+ */
+declare const getTextareaHeight: Function;
+export { getTextareaHeight, };
